6cb6dfd4b817aa32fa7eb4588301b9f738636c78
XIN-921102 <df321409@gmail.com>
2024-12-14 06:26:41 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-#載入LineBot所需要的套件
+# 載入 LineBot 所需要的套件
 from flask import Flask, request, abort
 
 from linebot import (
@@ -13,10 +13,9 @@ from linebot.models import *
 import re
 app = Flask(__name__)
 
-# 必須放上自己的Channel Access Token
+# 必須放上自己的 Channel Access Token
 line_bot_api = LineBotApi('RmscZ3tXPFTa3C+xKp9zU2zcapRysd2Lp/tRNkQT3a6FxxKY6XoTexhaMoarJVpf9X5PkvNRpFYLJJCpYJSlQfuPQ4VjgkuX46HOeXIv+fHJuqhaUGhSLXaWVsAqgVkY+zXzx40QYJL+d0GVK6BRQQdB04t89/1O/w1cDnyilFU=')
-
-# 必須放上自己的Channel Secret
+# 必須放上自己的 Channel Secret
 handler = WebhookHandler('dde3f81dc0ffc12b0b826d473d1c7fa3')
 
 line_bot_api.push_message('Ue67e135a8e71bb7f0a94eb6947e0dc32', TextSendMessage(text='你可以開始了'))
@@ -39,68 +38,41 @@ def callback():
 
     return 'OK'
 
-#訊息傳遞區塊
-##### 基本上程式編輯都在這個function #####
+# 訊息傳遞區塊
+##### 基本上程式編輯都在這個 function #####
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = text=event.message.text
-    if re.match('推薦景點',message):
-        carousel_template_message = TemplateSendMessage(
-            alt_text='熱門旅行景點',
-            template=CarouselTemplate(
-                columns=[
-                    CarouselColumn(
-                        thumbnail_image_url='https://i.imgur.com/xRBnJwk.png',
-                        title='法羅群島',
-                        text='Faroe Islands',
-                        actions=[
-                            URIAction(
-                                label='導覽',
-                                uri='https://maps.app.goo.gl/zTxbpMz9aTyEfAU77'
-                            ),
-                            URIAction(
-                                label='詳細資訊',
-                                uri='https://www.canon.com.hk/tc/club/article/itemDetail.do?itemId=10496'
-                            )
-                        ]
-                    ),
-                    CarouselColumn(
-                        thumbnail_image_url='https://i.imgur.com/ES4f9oQ.jpeg',
-                        title='冰島',
-                        text='Iceland',
-                        actions=[
-                            URIAction(
-                                label='導覽',
-                                uri='https://maps.app.goo.gl/NntWXAXjVxzsFuK49'
-                            ),
-                            URIAction(
-                                label='詳細資訊',
-                                uri='https://zh.wikipedia.org/zh-tw/%E5%86%B0%E5%B2%9B'
-                            )
-                        ]
-                    ),
-                    CarouselColumn(
-                        thumbnail_image_url='https://i.imgur.com/aRJXCWh.jpeg',
-                        title='北海道',
-                        text='Japan',
-                        actions=[
-                            URIAction(
-                                label='導覽',
-                                uri='https://maps.app.goo.gl/itU2dK1ccTTKaBENA'
-                            ),
-                            URIAction(
-                                label='詳細資訊',
-                                uri='https://zh.wikipedia.org/zh-tw/%E5%8C%97%E6%B5%B7%E9%81%93'
-                            )
-                        ]
-                    )
-                ]
-            )
+    message = event.message.text
+    if re.match('告訴我秘密', message):
+        flex_message = TextSendMessage(
+            text='請點選您想去的國家',
+            quick_reply=QuickReply(items=[
+                QuickReplyButton(action=MessageAction(label="日本", text="Japan")),
+                QuickReplyButton(action=MessageAction(label="台灣", text="Taiwan")),
+                QuickReplyButton(action=MessageAction(label="新加坡", text="Singapore")),
+                QuickReplyButton(action=MessageAction(label="韓國", text="Korea")),
+                QuickReplyButton(action=MessageAction(label="中國", text="China")),
+                QuickReplyButton(action=MessageAction(label="美國", text="US"))
+            ])
         )
-        line_bot_api.reply_message(event.reply_token, carousel_template_message)
+        line_bot_api.reply_message(event.reply_token, flex_message)
+    elif re.match('我想吃飯', message):
+        flex_message = TextSendMessage(
+            text='請選擇您想要的種類',
+            quick_reply=QuickReply(items=[
+                QuickReplyButton(action=MessageAction(label="主菜", text="主菜")),
+                QuickReplyButton(action=MessageAction(label="湯品", text="湯品")),
+                QuickReplyButton(action=MessageAction(label="飲料", text="飲料"))
+            ])
+        )
+        line_bot_api.reply_message(event.reply_token, flex_message)
+    elif message in ["主菜", "湯品", "飲料"]:
+        response_message = TextSendMessage(text=f"您已成功將【{message}】加入購物車")
+        line_bot_api.reply_message(event.reply_token, response_message)
     else:
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
-#主程式
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text="請輸入有效的指令"))
+
+# 主程式
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
